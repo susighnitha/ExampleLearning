2f212c4ebcc2406bb8b00865506829d0412c1954
spidugu <susighnithareddy00@gmail.com>
2024-04-01 13:38:22 +0530

    First commit

---
+++ src/collectininterfacemethods/Cinterfacemethods.java
@@ -0,0 +1,17 @@
+package collectininterfacemethods;
+
+import java.util.Vector;
+
+public class Cinterfacemethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Vector CF=new Vector();
+		CF.add("susighnitha");
+		CF.addAll("susighnitha,proya");
+		
+		System.out.println(CF);
+		
+	}
+}
